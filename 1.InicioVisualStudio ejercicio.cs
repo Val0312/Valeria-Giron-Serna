ae8d019be5bd3fa1ca9f15d8f3d683c1247ab4d3
Val0312 <valeria.giron@upb.edu.co>
2025-08-07 20:53:19 -0500

    Add files via upload

---
+++ 1.InicioVisualStudio ejercicio.cs
@@ -0,0 +1,30 @@
+﻿
+namespace _1.InicioVisualStudioejercicio;
+
+internal class Program
+{
+    static void Main(string[] args, Console Console)
+    {
+        //Esta es la declaración e inicialización de una variable
+        string nombre = "Valeria";
+        const string iva="19%";
+
+        console.Write("Hello, " + nombre + "\n");
+        Console.Write("\tBienvenido a la clase de programación");
+        string nombre = "Alejandro";
+        Console.Write("\n" + nombre);
+        //iva ="20%" ¡No se puede cambiar le valor de una constante!
+
+        //Tipos de datos
+        byte dato1 = 255;
+        int dato2 = 202321;
+        long dato3 = -920217989;
+        double dato4 = 320.625;
+        char dato5 = '$' ; //puede almacenar un solo caracter
+        bool dato6 = false;//Almacena true o false
+        object dato7 = new Program();
+    }
+
+
+    
+}
